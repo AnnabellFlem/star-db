57a1985169de09b6cb8dab3c06866605262c67f3
Anna Tsukanova <anna.tsukanova@kyivstar.net>
2020-11-05 23:04:06 +0200

    Updates

---
+++ precache-manifest.d3b9b512251bec9b0be17cb17be00087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b267ca43c71d9f59f51ab89bfcab3fc",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "17b3f242acb04c54c31f",
+    "url": "/star-db/static/css/main.d9e49482.chunk.css"
+  },
+  {
+    "revision": "f45ec6dad7d50a95e615",
+    "url": "/star-db/static/js/2.e23815a6.chunk.js"
+  },
+  {
+    "revision": "16b77b961957ed46d89c2958aecc8584",
+    "url": "/star-db/static/js/2.e23815a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b3f242acb04c54c31f",
+    "url": "/star-db/static/js/main.c574f621.chunk.js"
+  },
+  {
+    "revision": "dd843139110526c0b457",
+    "url": "/star-db/static/js/runtime-main.5b4e24ef.js"
+  }
+]);
